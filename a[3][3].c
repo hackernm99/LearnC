4168e3d215e8c30388d5b1c904459ebda3b99f70
Samm <36131946+hackernm99@users.noreply.github.com>
2019-10-28 11:03:12 +0530

    Create a[3][3].c
    Multi Multidimensional Array
    
    a[3][3]
    
    using For Loop

---
+++ a[3][3].c
@@ -0,0 +1,36 @@
+//Multi Multidimensional Array
+//Areeb Sayyed
+#include <stdio.h>
+void main()
+{
+    int i;
+    int j;
+    int a[3][3];
+
+    printf("Enter 3 Elements For Array : \n");
+
+    for (i = 0; i <= 2; i++)
+    {
+        for (j = 0; j <= 2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+        printf("Enter 3 Elements for next : \n");
+    }
+    printf("All Elements Filled : \n\n");
+
+    printf("Elements : \n");
+    for (i = 0; i <= 2; i++)
+    {
+        for (j = 0; j <= 2; j++)
+        {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/*
+
+*/
